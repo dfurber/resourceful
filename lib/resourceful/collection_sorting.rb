74cd55087064f7c8dd42bbce58168f4cb7a24d3d
David <furberd@gmail.com>
2012-04-25 12:25:10 -0400

    refactor default sort settings

--- lib/resourceful/collection_sorting.rb
+++ lib/resourceful/collection_sorting.rb
@@ -6,6 +6,14 @@ module Resourceful
       
       class_attribute :default_sort_column, instance_reader: true
       class_attribute :default_sort_direction, instance_reader: true
+
+      def self.set_default_sort_column(value)
+        self.default_sort_column = value
+      end
+      
+      def self.set_default_sort_direction(value)
+        self.default_sort_direction = value
+      end
       
       has_scope :sort_order, :only => :index, :using => [:c, :d], :default => {} do |controller, scoped, value| #, :default => [default_sort_column, default_sort_direction]
         if !value.first and !value.last
