466d78445ba4d7e14a11bd482608cd79dded133f
David <furberd@gmail.com>
2012-06-27 13:36:40 -0400

    update add_child_fields method

--- app/helpers/resourceful_helper.rb
+++ app/helpers/resourceful_helper.rb
@@ -171,10 +171,10 @@ module ResourcefulHelper
   
   def new_child_fields(form_builder, method, options = {})
     options[:object] ||= form_builder.object.class.reflect_on_association(method).klass.new
-    options[:partial] ||= 'base/' + method.to_s.singularize
+    options[:partial] ||= method.to_s.singularize
     options[:form_builder_local] ||= :form
     form_builder.fields_for(method, options[:object], :child_index => "new_#{method}") do |f|
-      render(:partial => options[:partial], :locals => { options[:form_builder_local] => f })
+      render options[:partial], options[:form_builder_local] => f
     end
   end
 
